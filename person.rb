225d98e4b5431b75b155ba71a5ba01b26c789c81
Jen Lim <jennolim@gmail.com>
2013-04-09 21:38:49 -0400

    Separate .rb files for student, instructor, and person

--- person.rb
+++ person.rb
@@ -1,28 +1,10 @@
-# class Person 
-#   attr_accessor :name
-#   attr_accessor :email
-#   attr_accessor :hometown
-#   attr_accessor :github_user
-#   attr_accessor :twitter
-#   attr_accessor :fun_fact
-
-#   def questions 
-
-   
-#   end
-# end
-
-require 'sqlite3'
-
 class Person
   include DataMapper::Resource 
-  # attr_accessor :id
-  # attr_accessor :type
-  # attr_accessor :name
-  # attr_accessor :email
-  property :id, serial
+  property :id, Serial
   property :type, Text, :required => true
   property :name, Text, :required => true
   property :email, Text, :required => true
-
+  property :twitter, Text
+  property :github, Text 
 end
+
--- server.rb
+++ server.rb
@@ -9,41 +9,35 @@ require 'dm-timestamps'
 require 'dm-sqlite-adapter'
 require 'webrick'
 require 'dm-validations'
+require 'pry'
 
 #db = SQLite3::Database.new("people.db")
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/peeps.db")
 
-#enable :sessions
+enable :sessions
 #use Rack::Flash
 
-# require_relative 'person'
-# require_relative 'student'
-# require_relative 'instructor'
-
-class Person
-  include DataMapper::Resource 
-  property :id, Serial
-  property :type, Text, :required => true
-  property :name, Text, :required => true
-  property :email, Text, :required => true
-
-end
+require_relative 'person'
+require_relative 'student'
+require_relative 'instructor'
 
 DataMapper.finalize.auto_upgrade! 
 
 
-before '/' do 
+before '/' do
   redirect '/login'
 end
 
 
 get '/' do
-  @people = Person.all :order => :id.desc
-  erb :success
+  people = Person.all :order => :id.desc
+  #session[:password] = params[:password]
+  erb :index
 end
 
 
 post '/login' do
+  #session[:password] = params[:password]
   unless params[:password] == "coolbeans"
    # flash[:notice] = "Sorry, wrong password"
     redirect '/login'
@@ -51,30 +45,20 @@ post '/login' do
   end
 end
 
-# post '/film' do
- 
-#   spacefix = params[:film].gsub(" ", "%20")
-#   movie = Movie.get_film_info(spacefix)
-
-#   erb :film, :locals => {:film => movie}
-# end
-
 get '/login' do
+  session[:password] = params[:password]
   erb :login
 end
 
 
 get '/student.erb' do
-  class Student < Person
   person = Student.new
-end
-  erb :student#, :locals => {:person => Person}
+  erb :student
 end
 
 get '/instructor.erb' do
-  class Student < Person
-  person = Instructor.new
-end
+#  class Student < Person
+    person = Instructor.new
   erb :instructor
 end
 
@@ -83,6 +67,9 @@ post '/success' do
   person.name = params[:name]
   person.email = params[:email]
   person.type = params[:type]
+  person.twitter = params[:twitter]
+  person.github = params[:github]
+  person.type
   person.save
   redirect '/'
  # erb :success
@@ -92,3 +79,11 @@ get '/success' do
     @people = Person.all :order => :id.desc
   end
 
+
+# post '/film' do
+ 
+#   spacefix = params[:film].gsub(" ", "%20")
+#   movie = Movie.get_film_info(spacefix)
+
+#   erb :film, :locals => {:film => movie}
+# end
